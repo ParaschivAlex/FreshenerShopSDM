f12c7ed3ea0441c9c0166322fdbfc4be2a85e358
222darkdark <56772112+ParaschivAlex@users.noreply.github.com>
2022-05-06 18:06:21 +0300

    About us frontend done, HOME PAGE REMAINING

--- FreshenerShopSDM/Controllers/HomeController.cs
+++ FreshenerShopSDM/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using FreshenerShopSDM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace FreshenerShopSDM.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
 		{
-			return View();
+            /*var fresheners = from ord in db.OrderCompletes
+                             orderby ord.FreshenerId.Count().Desc()
+                             group ord.FreshenerId                                
+                             select top 3 ord.FreshenerId;*/
+
+            IEnumerable<int> fresheners = db.OrderCompletes.GroupBy(oc => oc.FreshenerId).OrderByDescending(oc => oc.Count()).Take(3).Select(oc => oc.Key);
+            var top3Fresheners = db.Fresheners.Where(f => fresheners.Contains(f.FreshenerId)).ToList();
+            System.Diagnostics.Debug.WriteLine(fresheners);
+            ViewBag.MostPopular = top3Fresheners;
+            return View();
 		}
 
 		public ActionResult About()
